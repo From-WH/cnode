22e88ecc02ab09cb835c7221697b083d86dc07fc
wanghang <from-wh@hotmail.com>
2019-03-13 18:01:02 +0800

    上传node-modules

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/cnode/'
-    : '/'
+  configureWebpack: {
+    plugins: [
+      new MyAwesomeWebpackPlugin()
+    ]
+  }
 }
